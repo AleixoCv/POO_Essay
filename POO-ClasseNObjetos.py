585670cc502877df48ac65966e2d1af8796f1c82
Davi Aleixo <dac2@cesar.school>
2022-09-11 21:39:20 -0300

    new files

---
+++ POO-ClasseNObjetos.py
@@ -0,0 +1,13 @@
+# Criando a classe
+class Televisão:
+    def __init__(self, tela, polegadas) -> None:
+        self.tela = tela
+        self.polegadas = polegadas
+    
+
+# Instanciando os objetos (Gerando os objetos)
+SonyBravia = Televisão('LCD', 32)
+Samsung = Televisão('OLED', 24)
+
+# Aqui podemos printar na tela qualquer característica atribuida ao nosso objeto
+print(SonyBravia.polegadas)
--- POO-Main.py
+++ POO-Main.py
@@ -7,8 +7,25 @@ from abc import ABC, abstractmethod
 # Para definir uma classe, utilizamos
 class animal(ABC):
 
+    def __init__(self, raca):
+        self.raca = raca
+
     @abstractmethod
     def som(self):
         pass
 
+# Aqui podemos ver uma classe que herda da classes animal o atributo de raça e adiciona um atributo de nome
 class cachorro(animal):
+
+    def __init__(self, raca, nome):
+        self.nome = nome
+
+    def som(self):
+        print("Latido - Au Au")
+
+# Definindo um objeto
+objeto = cachorro('cachorro', 'Arya')
+
+# Chamando um método dentro de uma classe. Também é importante perceber que o método só é chamado pois a classe cachorro
+# atende aos requisitos de sua SuperClasse, ou classe mãe, de ter um método "som", senão haveria um erro
+objeto.som() # O output dessa chamada será "Latido - Au Au".
